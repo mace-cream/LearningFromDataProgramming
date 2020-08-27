a6b050a7f86f9d70237e5b9b84a4d56e2e51e6eb
zhaofeng-shu33 <616545598@qq.com>
2020-08-27 16:45:25 +0800

    config shell

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 export PATH=$PATH:$HOME/.poetry/bin
+source $HOME/.poetry/env
 poetry install
