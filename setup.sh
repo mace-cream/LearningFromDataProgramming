b80ea30a12e9b04d7bcd0f84e3aaa54c364e4e60
zhaofeng-shu33 <616545598@qq.com>
2020-08-27 16:52:48 +0800

    use action from marketplace

--- setup.sh
+++ setup.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
-export PATH=$PATH:$HOME/.poetry/bin
-source $HOME/.poetry/env
 poetry install
